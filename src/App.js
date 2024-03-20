6cdeecf7511a3380d955fd9d412cf978b8b17324
pavansailyros <pavansai@lyrostech.com>
2024-03-20 12:35:04 +0530

    added apis

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import { useState, createContext, useContext } from "react";
-import Login from "./Login";
-import Home from "./Home";
-export const UserContext = createContext();
+import React, {useContext}  from 'react'
+import { UserContext } from './components/UserContext/Usercontext'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from './components/home/Home'
+import Login from './components/user/Login'
+import Register from './components/user/Register'
+import Feeds from './components/feeds/Feeds';
+import Albums from './components/albums/Albums';
+
+
+// We should call login comp and register 
 export default function App() {
-  const [user, setUser] = useState("John");
-  const [flag, setFlag] = useState(0);
- 
+    const  {flag, setFlag} =useContext(UserContext);
+
   return (
-    <>
-      <UserContext.Provider value={{ user, setUser, flag, setFlag } }>
-       
-        {flag==0||flag==1?<Login/>:<Home/>}
-     
-      </UserContext.Provider>
-    </>
-  );
+    <div>
+        <Router>
+        {flag == 0 || flag==2 ? <Login/>:<Home />}
+        {flag ===2 && <Register/>}
+        
+        <Routes>
+            <Route path="/feeds" element={<Feeds />}></Route>
+            <Route path="/albums" element={<Albums />}></Route>
+        </Routes>
+        </Router>
+        
+    </div>
+  )
 }
---
+++ src/components/albums/Albums.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useState,useEffect } from 'react';
+import Images from './Images';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+
+export default function Albums() {
+  const [data,setData] = useState([]);
+ const url ="https://jsonplaceholder.typicode.com/albums"
+
+ const handleClick=()=>{
+  <Routes>
+  <Route path="/albums" element={<Images />}></Route>
+</Routes>
+ 
+ }
+
+ const fetchData =async (url) => {
+  try {
+    const response =await fetch(url);
+    const result =await response.json();
+    setData(result);
+  }
+  catch (err) {
+    console.log(err);
+  }
+ }
+ useEffect(() =>{
+  fetchData(url)
+ },[])
+  return (
+    <div>
+      {data && data.map((v,i) => <div key={v.id} onClick={handleClick}><i>{v.title}</i><br/><br/></div>)}
+    </div>
+  )
+}
